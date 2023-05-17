ae313b17aba117aef9a8453cea5ecb0297f0f91b
EvertonBMoraes <evertonbarrosdemoraes@gmail.com>
2023-05-17 20:58:49 -0300

    Pequenos Ajustes

--- Aula5/src/App.jsx
+++ Aula5/src/App.jsx
@@ -2,7 +2,11 @@ import Produtos from "./components/Produtos";
 import './App.css'
 
 function App() {
-  return <Produtos />;
+  return (
+  <>
+  <p className="itens">Incríveis Descontos</p>
+  <Produtos />
+  </>)
 }
 
 export default App;
