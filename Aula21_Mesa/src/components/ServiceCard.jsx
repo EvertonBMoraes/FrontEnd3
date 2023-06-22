564ec594b058168d033962157f9ec75223bbff75
EvertonBMoraes <evertonbarrosdemoraes@gmail.com>
2023-06-21 23:04:21 -0300

    Mesa 21 entregue

---
+++ Aula21_Mesa/src/components/ServiceCard.jsx
@@ -0,0 +1,47 @@
+import { useContext } from "react";
+
+import ServiceContext from "../contexts/ServiceContext";
+
+
+function ServiceCard({ service }) {
+
+    const { services, dispatch } = useContext(ServiceContext); 
+
+
+    const handleDeleteService = (id) => {
+    
+    dispatch(
+        {
+            type: "REMOVE_SERVICE",
+            payload: id
+        }
+    );
+
+    }
+
+    return (
+
+        <div className='divServices'>
+
+            <div className='serviceType'>
+                <div className="serviceLabel">Atendimento</div>
+                {service.type}
+            </div>
+
+            <div className='serviceTime'>
+                <div className="serviceLabel">Tempo estimado</div>
+                {service.time}
+            </div>
+
+            <button style={{
+                backgroundColor: "#8a6df1",
+                color: "#2F2D2D"
+            }} onClick={() => handleDeleteService(service.id)}>Remover
+            </button>
+
+        </div>
+
+    );
+}
+
+export default ServiceCard;
